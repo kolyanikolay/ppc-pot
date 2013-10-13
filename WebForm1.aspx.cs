45577cc54547b2c2a8385dcbb5ff94fc92bc9fec
Колян <xa4tryka4@gmail.com>
2013-10-13 08:25:28 -0700

    Create WebForm1.aspx.cs

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public Dictionary<string,string> collection = new Dictionary<string, string>();
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="q";
+            
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="w";
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="e";
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="r";
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="t";
+        }
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="y";
+        }
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="u";
+        }
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="i";
+        }
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="o";
+        }
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="p";
+        }
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="a";
+        }
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="s";
+        }
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="d";
+        }
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="f";
+        }
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="g";
+        }
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="h";
+        }
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="j";
+        }
+        protected void Button17_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="k";
+        }
+        protected void Button18_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="l";
+        }
+        protected void Button19_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="z";
+        }
+        protected void Button20_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="x";
+        }
+        protected void Button21_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="c";
+        }
+        protected void Button22_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="v";
+        }
+        protected void Button23_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="b";
+        }
+        protected void Button24_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="n";
+        }
+        protected void Button25_Click(object sender, EventArgs e)
+        {
+            tb1.Text +="m";
+        }
+    }
+}
